d7665a7b7a226ea8326904623709b2fd8df9b03d
André A2 Engenharia <contato@a2ei.com.br>
2026-05-27 20:46:02 -0300

    Bump service worker cache para forçar atualização do código

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='triboltec-v1';
+const CACHE='triboltec-v3';
 const ASSETS=['/','/index.html','/manifest.json',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'];
